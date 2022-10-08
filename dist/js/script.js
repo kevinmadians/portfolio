fb7c691d91262bd07abc4f9a0b4a6617a79dae75
kevinmadians <kevinmadians@gmail.com>
2022-10-08 19:44:52 +0700

    perubahan kedua

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,18 @@
 // Navbar Fixed
 window.onscroll = function() {
-    const header = document.querySelector('header')
+    const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
